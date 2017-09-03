e2a53bc5798151e68895a0d300643e9600fabf82
Sam Benallouch <ossama.web@gmail.com>
2017-09-03 18:16:12 +0100

    ignore manual inputs

--- android/src/main/java/com/reactnative/googlefit/StepHistory.java
+++ android/src/main/java/com/reactnative/googlefit/StepHistory.java
@@ -267,14 +267,20 @@ public class StepHistory {
             Log.i(TAG, "\t\tStart: " + dateFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
             Log.i(TAG, "\t\tEnd  : " + dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS)));
 
-            for(Field field : dp.getDataType().getFields()) {
-                Log.i(TAG, "\t\tField: " + field.getName() +
-                        " Value: " + dp.getValue(field));
+            String originalDsIdentifier =  dp.getOriginalDataSource().getStreamIdentifier();
 
-                stepMap.putDouble("startDate", dp.getStartTime(TimeUnit.MILLISECONDS));
-                stepMap.putDouble("endDate", dp.getEndTime(TimeUnit.MILLISECONDS));
-                stepMap.putDouble("steps", dp.getValue(field).asInt());
-                map.pushMap(stepMap);
+            Boolean isUserInput = originalDsIdentifier.matches("(?i).*:user_input.*");
+
+            if (!isUserInput) {
+                for(Field field : dp.getDataType().getFields()) {
+                    Log.i(TAG, "\t\tField: " + field.getName() +
+                            " Value: " + dp.getValue(field));
+
+                    stepMap.putDouble("startDate", dp.getStartTime(TimeUnit.MILLISECONDS));
+                    stepMap.putDouble("endDate", dp.getEndTime(TimeUnit.MILLISECONDS));
+                    stepMap.putDouble("steps", dp.getValue(field).asInt());
+                    map.pushMap(stepMap);
+                }
             }
         }
     }
